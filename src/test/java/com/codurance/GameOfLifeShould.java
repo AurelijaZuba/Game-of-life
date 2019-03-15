8704f4d9302d0e7ce0d37b71017739b7f37ef1a6
Raul Mordillo <raul.mordillo@codurance.com>
2019-03-15 12:39:13 +0100

    start over again

--- src/test/java/com/codurance/GameOfLifeShould.java
+++ src/test/java/com/codurance/GameOfLifeShould.java
@@ -5,13 +5,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GameOfLifeShould {
-    //Any live cell with fewer than two live neighbours
-    // dies, as if caused by under-population.
 
-    @Test
-    void aCellDieWhenThereAreLessThan2LiveNeighbours() {
 
-        Cell cell = new Cell();
-        assertThat(cell.status(), is (CellStatus.DEAD));
-    }
 }
